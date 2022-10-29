25c19d4047e5be955926176b7f66aaadfe8e0027
AktoFredy <workmailAkto@gmail.com>
2022-10-29 23:57:06 +0700

    Guided 8 Accelerometer FIX

---
+++ app/src/main/java/com/example/gd8_accelerometer_d_10778/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.gd8_accelerometer_d_10778
+
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity(), SensorEventListener{
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Keeps phone in light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        square = findViewById(R.id.tv_square)
+
+        setUpSensorStuff()
+    }
+
+    private fun setUpSensorStuff(){
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        //specify the sensor you want to listen to
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // Checks for sensor we have registered
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            // Log.d ("Main", "onSensorChanged: sides ${event.values[0]} front/back ${event.values[1]} ")
+
+            //Sides = Tilting phone left(10) and right(-10)
+            val sides = event.values[0]
+
+            // Up/Down = Tilting phone up(10), flat (0), upside-down(10)
+
+            val upDown = event.values[1]
+
+            square.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * 10
+            }
+
+            //Changes the colour of the square if it's completely flat
+            val color = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+            square.setBackgroundColor(color)
+
+            square.text = "up/down ${upDown.toInt()}\nleft/right${sides.toInt()}"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
+}
